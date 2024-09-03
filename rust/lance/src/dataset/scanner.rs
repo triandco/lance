ca8ebbed889057794947bc1b8cea052702c94c19
Tri <tri@triandco.com>
2024-09-03 04:43:33 +0000

    feat: add u8 supports

--- rust/lance/src/dataset/scanner.rs
+++ rust/lance/src/dataset/scanner.rs
@@ -453,6 +453,7 @@ impl Scanner {
                     DataType::Int16 => coerce_float_vector(q, FloatType::try_from(&DataType::Float32)?)?,
                     DataType::Int32 => coerce_float_vector(q, FloatType::try_from(&DataType::Float32)?)?,
                     DataType::Int64 => coerce_float_vector(q, FloatType::try_from(&DataType::Float64)?)?,
+                    DataType::UInt32 => coerce_float_vector(q, FloatType::try_from(&DataType::Float32)?)?,
                     _ => {
                         return Err(Error::io(
                             format!(
